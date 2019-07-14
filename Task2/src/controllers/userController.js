d1fe961761f22775b2d4586d3672d893975418f6
Anastasiia Plutalova <anastasiia_plutalova@epam.com>
2019-07-14 14:12:12 +0400

    feat(router): setup routes for user

--- Task2/src/controllers/userController.js
+++ Task2/src/controllers/userController.js
@@ -2,6 +2,22 @@ class UserController {
     getUsers = (req, res) => {
         res.send('Get all users from service!!!');
     }
+
+    getUserById = (req, res) => {
+        res.send(req.params.id);
+    }
+
+    createUser = (req, res) => {
+        res.send('create');
+    }
+
+    updateUser = (req, res) => {
+        res.send('update');
+    }
+
+    deleteUser = (req, res) => {
+        res.send('delete');
+    }
 }
 
 export default new UserController();
--- Task2/src/routes/userRoutes.js
+++ Task2/src/routes/userRoutes.js
@@ -5,4 +5,12 @@ const router = express.Router();
 
 router.get('/', UserController.getUsers);
 
+router.get('/:id', UserController.getUserById);
+
+router.post('/', UserController.createUser);
+
+router.put('/', UserController.updateUser);
+
+router.delete('/:id', UserController.deleteUser);
+
 export default router;
